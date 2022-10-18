3fd49c6c007863f90c2fe0336395ed61c4b9f142
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-18 16:22:13 +0300

    Fix Comments for tasks

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -4,7 +4,8 @@
 456 -> 5
 782 -> 8
 918 -> 1
-
+*/
+/*
 int ReturnSecondFigure(int number)
 {
     int cutThirdFigure = number / 10;
@@ -27,38 +28,41 @@ Console.Write($"The second figure of number {number} is {result}");
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6
-
+*/
+/*
 int ReturnThirdFigure(int number)
 {
-    if (number >= 100)
+    if (number >= 100) //Проверяем на кол-во цифр, если их больше 3, т.е. число больше 100
      {
-        int countFigures = 3;
-        while (number / Convert.ToInt32(Math.Pow(10, countFigures)) > 0)
+        int countFigures = 3; //счетчик цифр в числе
+        while (number / Convert.ToInt32(Math.Pow(10, countFigures)) > 0) // Цикл будет работать пока не дойдем до трехзначного числа. Math.Pow(10 в степени счетчика цифр)
             countFigures++;
         int thirdFigure = number % Convert.ToInt32(Math.Pow(10, countFigures - 2)) 
-        / Convert.ToInt32(Math.Pow(10, countFigures - 3));
+        / Convert.ToInt32(Math.Pow(10, countFigures - 3)); // Избавляемся от лишних правых и левых цифр с помощью целочисленного деления и остатка от деления. Чтобы искать другие цифры в числе, например вторую или шестую, то в последней скобке укажим вместо -3, тот номер числа который будем искать, в первой скобке- предпослднее число от искомого)
         return thirdFigure;
      }
-     else return -1;
+     return -1; //Вернем ошибку, если введено меньшее число
 }
 Console.WriteLine("Input any number ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int result = ReturnThirdFigure(number);
 
+
 if (result == -1)
     Console.WriteLine("This number hasn't the third figure");
 else    
     Console.WriteLine($"The third figure of number {number} is {result}");
+*/
 
-
-// Задача 3. Напишите программу, которая принимает на вход цифру, обозначающую 
+/* Задача 3. Напишите программу, которая принимает на вход цифру, обозначающую 
 //день недели, и проверяет, является ли этот день выходным.
 
 6 -> да
 7 -> да
 1 -> нет
-
+*/
+/*
 bool IsWeekEnd(int day)
 {
     if (day ==6 || day == 7 ) return true;
@@ -70,5 +74,7 @@ int day = Convert.ToInt32(Console.ReadLine());
 
 bool result = IsWeekEnd(day);
 
-Console.WriteLine($"The day you input is weekend. It's {result} ");
+//Вывод булевого итога Console.WriteLine (результат какой? Если true "Text 1" : если False "Text 2")
+Console.WriteLine (result ? "The day you input is weekend" : "The day is you input isn't weekend");
+
 */
