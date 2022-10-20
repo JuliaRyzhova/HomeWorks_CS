04caef0b742d2905251bccb58dd8613dabaf6d9f
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-20 10:03:27 +0300

    need continue first task

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+//Например:
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+bool IsPalindrome(int number)
+    //if (number => 10000 || number < 100000)
+        {
+            if (number / 10000 == number % 10)
+            {
+                if (number / 1000 == number / 10 % 10)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    //return -1;
+
+Console.WriteLine ("Input the five digit number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsPalindrome(number);
+
+//if (result == -1)
+//Console.WriteLine("Invalid number");
+Console.WriteLine(result ? "Tne number is palindrome" : "The number isn't palindrome");
+
