3b72591f2777161e8cc80dc2a1fb0276e3c84903
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-27 18:14:32 +0300

    Not working

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -48,13 +48,15 @@ Console.WriteLine($"{a} to the degree of {b} is {result}");
 82 -> 10
 9012 -> 12
 */
+
 int Sum(int number)
 {
     int result = 0;
-    while (number >= 0 )
+    while (number >= 0)
     {
         result += number % 10;
         number /= 10;
+
     }
     return result += number;
 }
@@ -62,6 +64,7 @@ Console.WriteLine("Input any positiv number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int result = Sum(number);
+Console.WriteLine(result);
 
 
 
