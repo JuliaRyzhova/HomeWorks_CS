6fed61e0ab5c66ff621133a42988d508517ac483
Julia Ryzhova <16-j.lazy@bk.ru>
2022-10-27 18:25:53 +0300

    Finish today

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -54,11 +54,10 @@ int Sum(int number)
     int result = 0;
     while (number >= 0)
     {
-        result += number % 10;
-        number /= 10;
-
+        result = result + number % 10;
+        number = number / 10;
     }
-    return result += number;
+        return result;
 }
 Console.WriteLine("Input any positiv number: ");
 int number = Convert.ToInt32(Console.ReadLine());
